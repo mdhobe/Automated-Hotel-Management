cd06556a34488a1e8eb99c39c3be7897f33480de
mdhobe <37969422+mdhobe@users.noreply.github.com>
2021-02-14 18:34:33 +0530

    Automatic Table Number generate

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
+
 @app.route('/', methods=['GET'])
 def login():
     return render_template('login.html')
